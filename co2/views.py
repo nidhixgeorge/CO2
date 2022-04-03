2365630ddee8ba80d93a92cf1b6633c3bfcfa970
Vivek N <vivekbiz@gmail.com>
2022-04-02 23:21:11 -0700

    html changes

--- co2/views.py
+++ co2/views.py
@@ -4,12 +4,23 @@ from .forms import CarbonForm1, CarbonForm2
 
 # Create your views here.
 def homepage(request):
-
-    form1 = CarbonForm1()
-    form2 = CarbonForm2()
-    c = {
+    if request.method == 'POST':
+        form1 = CarbonForm1()
+        form2 = CarbonForm2()
+        msg = "We are working on calculating your carbon footprints"
+        c = {
                     'form1' : form1,
                     'form2' : form2, 
                                       
                     }
-    return render(request, 'co2/homepage.html', c)
+        return render(request, 'co2/homepage.html', c)   
+    else:
+        form1 = CarbonForm1()
+        form2 = CarbonForm2()
+        
+        c = {
+                        'form1' : form1,
+                        'form2' : form2, 
+                                        
+                        }
+        return render(request, 'co2/homepage.html', c)
--- smud2022/settings.py
+++ smud2022/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#v5zp^%@_qj#4*=7n!5=9@jcy%gnm3)o6wj5q&qr0z=4fy#4+n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://co2reduction.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'selectable',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'smud2022.wsgi.application'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticui",
+    
+    
+]
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
